d0b054dbacf219108e652e583409fa3413cdc8e2
DerkachEkaterina <derkachekaterina12345@gmail.com>
2022-10-12 17:51:12 +0200

    js with negative nums

--- 1.js
+++ 1.js
@@ -1,26 +1,20 @@
 let num1 = 0;
 let num2 = 0;
-let finish = false;
+
 let res = 0;
 let sign = '';
-function clear(){
-    res = 0;
-    num1 = 0;
-    num2 = 0;
-    sign = '';
-    document.getElementById('out').innerHTML = 'res';
-
-}
+let flag1;
+let flag2;
 
 function num(n){
-    if (sign == ''){
+    if(num1 === 0 && num2 === 0 && sign === '' && flag1 !== -1)
+        document.getElementById('out').innerHTML = '';
+    if (sign === ''){
         num1 = parseInt(num1)*10+parseInt(n);
-        document.getElementById('out').innerHTML = num1;
-    }
-
-     else{
+        document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + num1%10;
+    }else{
         num2 = parseInt(num2)*10+parseInt(n);
-        document.getElementById('out').innerHTML = num2;
+        document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + num2%10;
     }
 
 
@@ -28,33 +22,63 @@ function num(n){
 
 function plus(){
     sign = '+';
+    document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + "+";
 }
 function minus(){
-    sign = '-';
+    if(num1 !== 0 && num2 === 0 && sign !== ''){
+        flag2 = -1;
+        document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + "(-";
+    }else
+        if(num1 === 0 && num2 === 0 && sign === ''){
+            flag1 = -1;
+            document.getElementById('out').innerHTML = '-';
+
+    } else{
+            sign = '-';
+            document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + "-";
+        }
+
 }
 function mult(){
     sign = '*';
+    document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + "*";
 }
 function div(){
     sign = '/';
+    document.getElementById('out').innerHTML = document.getElementById('out').innerHTML + "/";
 }
 function isEqual(){
+    if(flag1 !== -1)
+        flag1 = 1;
+    if(flag2 !== -1)
+        flag2 = 1;
+
     if(sign === '+')
-        res = num1+num2;
+        res = flag1*num1+flag2*num2;
     if(sign === '-')
-        res = num1-num2;
+        res = flag1*num1-flag2*num2;
     if(sign === '*')
-        res = num1*num2;
+        res = (flag1*num1)*(flag2*num2);
     if(sign === '/') {
         if (num2 !== 0) {
-            res = num1 / num2;
-        } else res = 0;
+            res = (flag1*num1)/(flag2*num2);
+        } else res = "error";
     }
 
     num1 = 0;
     num2 = 0;
     sign = '';
+    flag1 = 1;
+    flag2 = 1;
     document.getElementById('out').innerHTML = res;
     res = 0;
+}
+function clear(){
+    res = 0;
+    num1 = 0;
+    num2 = 0;
+    sign = '';
+    document.getElementById('out').innerHTML = '';
+
 }
 
